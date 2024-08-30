8079caf8e58e6a715c6e37924ef3fbf05ca41e9a
bootloopmaster636 <christhartono@hotmail.com>
2024-08-30 10:03:56 +0700

    fix: split utility to util. widget and util. function

---
+++ lib/panel/logic/utility_function.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:kitshell/const.dart';
+import 'package:toastification/toastification.dart';
+
+void showToast({
+  required BuildContext context,
+  required String message,
+  IconData? icon,
+  // ImageProvider? image,
+}) {
+  toastification.showCustom(
+    autoCloseDuration: const Duration(seconds: 3),
+    alignment: Alignment.centerRight,
+    animationDuration: const Duration(milliseconds: 800),
+    animationBuilder: (context, animation, alignment, child) {
+      return SlideTransition(
+        position: animation.drive(
+          Tween(begin: const Offset(0, 1), end: Offset.zero).chain(
+            CurveTween(curve: Curves.easeOutExpo),
+          ),
+        ),
+        child: child,
+      );
+    },
+    builder: (BuildContext context, ToastificationItem holder) {
+      return Container(
+        height: panelHeight,
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surfaceContainerHigh,
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Row(
+          children: [
+            if (icon != null) Icon(icon),
+            if (icon != null) const Gap(8),
+            Text(message),
+          ],
+        ),
+      );
+    },
+  );
+}
