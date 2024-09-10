727d9009a1e441d1d33639ce44765d58efa398d5
bootloopmaster636 <christhartono@hotmail.com>
2024-09-10 16:09:05 +0700

    fix: reduce expanded menu width once more

--- lib/const.dart
+++ lib/const.dart
@@ -1,4 +1,4 @@
 const int expandedSubmenuHeightMultiplier = 8;
-const double expandedSubmenuContentWidth = 880;
+const double expandedSubmenuContentWidth = 820;
 const Duration pollingRate = Duration(milliseconds: 1000);
 const Duration toastDuration = Duration(seconds: 3);
