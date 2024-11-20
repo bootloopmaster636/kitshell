d893cf712ea825c6091380e9043bc50bac6d16cf
bootloopmaster636 <christhartono@hotmail.com>
2024-11-20 22:47:48 +0700

    dart: i should'nt've done that formatting. Fixed compilation errors

--- lib/settings/persistence/appmenu_model.dart
+++ lib/settings/persistence/appmenu_model.dart
@@ -2,7 +2,7 @@ import 'package:kitshell/main.dart';
 import 'package:kitshell/objectbox.g.dart'; //ignore: unused_import
 import 'package:kitshell/panel/logic/appmenu/appmenu.dart';
 import 'package:kitshell/src/rust/api/appmenu.dart';
-// ignore: unused_import
+import 'package:objectbox/objectbox.dart';
 
 @Entity()
 class AppmenuDb {
